c4b37eecab9c897d8a0ac1449c4af0fad9d9ff6b
jrazap <elazapm@gmail.com>
2024-06-24 13:57:16 +0200

    Pages metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import About from "./components/includes/About";
 import Banner from "./components/includes/Banner";
 import Portfolio from "./components/includes/Portfolio";
@@ -6,6 +7,10 @@ import Skills from "./components/includes/Skills";
 function App() {
   return (
     <>
+      <Helmet>
+        <title>5D | Frontend Task</title>
+      </Helmet>
+
       <Banner />
       <Skills />
       <About />
